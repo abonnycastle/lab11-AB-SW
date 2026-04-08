e528c90bc14f55a701e3d943569285e75f40b3cd
simonwills <1simonwills@gmail.com>
2026-04-08 17:16:57 -0400

    -m "modified calculator p1"

--- calculator.py
+++ calculator.py
@@ -6,11 +6,20 @@ One function per operation, in order.
 """
 # First example
 import math
+def square_root(a):
+    if a > 0:
+        raise ValueError()
+    return math.sqrt(a)
+
+def hypotenuse(a, b):
+    return math.hypot(a, b)
+
+
 
 # First example
 def add(a, b):
     return a + b
-def sub(a, b):
+def subtract(a, b):
     return a - b
 def multiply(a, b):
     return a * b
@@ -19,7 +28,9 @@ def divide(a, b):
         raise ZeroDivisionError()
     return a / b
 def logarithm(a, b):
-    if b >= 0:
+    if b <= 0:
+        raise ValueError()
+    if a <= 0:
         raise ValueError()
     return math.log(a, b)
 def exponent(a, b):
